6377c5eba5427a3934fc95f860e154ccb53a66d4
Joe Galipeau <joejoe4223@gmail.com>
2024-08-23 15:05:04 -0700

    modified update so it can update any collection, added read_raw to read all documents from full_data

--- Mongo.py
+++ Mongo.py
@@ -1,16 +1,15 @@
 import pymongo
-import main
 import json
 
 # Connect to MongoDB
 
 
-def update(new_data):
+def update(new_data, collectionName="full_data"):
     client = pymongo.MongoClient("mongodb://mongoadmin:pass@localhost:27017/")
 
     database = client["Wa_Lotto"]
 
-    collection = database["full_data"]
+    collection = database[collectionName]
     try:
         collection.insert_many(new_data)
         print("successful")
@@ -20,5 +19,19 @@ def update(new_data):
     return None
 
 
+def read_raw():
+    # Gets all documents from the full_data collection
+    client = pymongo.MongoClient("mongodb://mongoadmin:pass@localhost:27017/")
+
+    database = client["Wa_Lotto"]
+
+    collection = database["full_data"]
+
+    documents = list(collection.find())
+
+    return documents
+
+
 # Call the update function with the fake car data
 # update(database, main.get_data())
+read_raw()
