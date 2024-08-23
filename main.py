dc76d6a5754308e7532806467afcddafdbfaabbb
Joe Galipeau <joejoe4223@gmail.com>
2024-08-23 15:04:23 -0700

    added bs4 to scrape rendered html (tickets sold) and added EV calculations

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import pandas as pd
 from requests import get
 from bs4 import BeautifulSoup
 from datetime import datetime
-import pprint
-import os
-import json
 import Mongo
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import warnings
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
 # function to scrape data from website
@@ -16,7 +20,10 @@ import Mongo
 
 
 def get_data():
+    options = webdriver.SafariOptions()
+
     dict_list = []
+    game_id_list = []
 
     urls_list = [
         "https://www.walottery.com/Scratch/TopPrizesRemaining.aspx?price=$1",
@@ -41,22 +48,96 @@ def get_data():
             data_table = pd.read_html(str(table.select("div > table")[0]))
             # print((data_table[0]))
             Last_day = table.select("div > header > div > p:nth-child(3)")[0].text
+
+            # -------------------------------
+
+            game_id = table.select("div > header > div > p:nth-child(2)")[0].text[-4:]
+
+            url = f"https://www.walottery.com/Scratch/Explorer.aspx?id={game_id}"
+            driver = webdriver.Safari(options=options)
+            driver.get(url)
+            wait = WebDriverWait(driver, 10)
+            r = wait.until(
+                lambda driver: driver.find_element(
+                    By.XPATH,
+                    '//*[@id="global-content"]/section[2]/div/div/div[1]/div/div[2]/div[2]/div[1]/div[2]/p[1]/strong',
+                ).text
+                != "N/A"
+            )
+            r = driver.find_element(
+                By.XPATH,
+                '//*[@id="global-content"]/section[2]/div/div/div[1]/div/div[2]/div[2]/div[1]/div[2]/p[1]/strong',
+            )
+            tickets_sold = int(r.text.replace(",", ""))
+            driver.quit()
+
+            # --------------------------------------------------------------------
+            # the value tickets sold is rendered using javascript. I may need to re scrape everything using selenium instead:/
             dictionary = {
                 "date": datetime.today().strftime("%m/%d/%Y"),
                 "game_name": game_name,
                 "game_price": game_price,
                 "data_table": data_table[0].to_dict("records"),
                 "Last_day": Last_day,
+                "tickets_sold": tickets_sold,
             }
+            print("table")
             dict_list.append(dictionary)
+        print("url")
+
     return dict_list
 
 
-# to do: add mongo db for all data
-#       Add time stamp for each data
-#       do calculations
+def calculate():
+    documents = Mongo.read_raw()
+    EV_dict_list = []
+
+    for doc in documents:
+        try:
+            df = pd.DataFrame(doc["data_table"])
+            tickets_sold = doc["tickets_sold"]
+            ticket_price = float(doc["game_price"].split()[0].replace("$", ""))
+            new_prize_amount = "0"
+            new_total_prizes = tickets_sold - sum(df["Total Prizes"])
+            new_prizes_remaining = new_total_prizes - sum(df["Prizes Remaining"])
+            new_prizes_paid = new_total_prizes - new_prizes_remaining
+            zero_row = {
+                "Prize Amount": new_prize_amount,
+                "Total Prizes": new_total_prizes,
+                "Prizes Paid": new_prizes_paid,
+                "Prizes Remaining": new_prizes_remaining,
+            }
+            df["Prize Amount"] = (
+                df["Prize Amount"].str.replace("$", "").str.replace(",", "")
+            )
+            df.loc[len(df)] = zero_row
+
+            df["Probabilty"] = df["Prizes Remaining"] / sum(df["Prizes Remaining"])
+
+            df["Prize Amount"] = pd.to_numeric(df["Prize Amount"])
+
+            df["Expected Value"] = df["Prize Amount"] * df["Probabilty"]
+
+            EV = sum(df["Expected Value"]) - ticket_price
+            print(f"{doc['game_name']} on {doc['date']} has an EV of {EV}")
+
+            # Adding it to the database
+            EV_dict = {
+                "game_name": doc["game_name"],
+                "date": doc["date"],
+                "EV": EV,
+                "ticket_price": ticket_price,
+                "tickets_sold": tickets_sold,
+            }
+            EV_dict_list.append(EV_dict)
+        except:
+            print(f"{doc['game_name']} on {doc['date']} -- Failed")
+
+    Mongo.update(new_data=EV_dict_list, collectionName="EV_data")
+    return None
 
 
 if __name__ == "__main__":
-    data_full = get_data()
-    Mongo.update(data_full)
+    # data_full = get_data()
+    # Mongo.update(data_full, collectionName="full_data")
+    calculate()
